a4e76bfa2f366edd1bcd9940b56a8f9b406537ca
SAHIDUL-ISLAM <sahidul99622@gmail.com>
2026-02-02 21:44:26 +0600

    count how many times a number repeated solution

--- js-solution.js
+++ js-solution.js
@@ -1,9 +1,54 @@
 // Task-1:
 // Write a function to convert temperature from Celsius to Fahrenheit.
 
-function celsiusToFahrenheit(celsious){
-    let Fahrenheit = (celsious*1.8)+32;
-    return Fahrenheit;
+// function celsiusToFahrenheit(celsious){
+//     let Fahrenheit = (celsious*1.8)+32;
+//     return Fahrenheit;
+// }
+// const result = celsiusToFahrenheit(10);
+// console.log(result);
+
+
+
+
+
+// ********************
+
+
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+// sample-input: numbers = [5,6,11,12,98, 5]
+
+// find: 5
+
+// output: 2
+
+
+// sample-input:
+
+// numbers = [5,6,11,12,98, 5]
+
+// find: 25
+
+// output: 0
+
+function repeatedNum(arr){
+    let count = 0;
+    for(let numOfArr of arr ){
+        if(numOfArr === 25){
+            count++;
+        }
+    }
+    return count;
 }
-const result = celsiusToFahrenheit(10);
+const numbers = [5,6,11,12,98,2];
+const result = repeatedNum(numbers);
 console.log(result);
+
+
+
+
+
+
+// ********************
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+    for(let numOfArr of arr ){
+        if(numOfArr.includes(25) === true){
+            count = stor + count;
+        }
+    }
+}
